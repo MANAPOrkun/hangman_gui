c88a3bf3c57036c339fe3f4678d374c3d17ff960
Orkun Manap <manaporkun@protonmail.com>
2021-09-23 11:16:26 +0300

    UI Update - Word Structure

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use gio::prelude::*;
 use gtk::prelude::*;
-use rand::Rng;
 use std::env;
+use glib::clone;
+use std::{cell::Cell, rc::Rc};
+use rand::Rng;
+use glib::clone::Downgrade;
 
 fn main() {
     let app = gtk::Application::new(
@@ -16,67 +19,120 @@ fn main() {
         let window: gtk::Window = builder.get_object("application_window").unwrap();
         window.set_application(Some(app));
 
-        // let score_label: gtk::Label = builder.get_object("score_label").unwrap();
+        // Get UI elements
+        let score_label: gtk::Label = builder.get_object("score_label").unwrap();
         let point_label: gtk::Label = builder.get_object("score_point").unwrap();
         let word_label: gtk::Label = builder.get_object("word_label").unwrap();
         let check_button: gtk::Button = builder.get_object("check_button").unwrap();
         let text_entry: gtk::Entry = builder.get_object("text_entry").unwrap();
         let hangman_image: gtk::Image = builder.get_object("hangman_image").unwrap();
-        let hangman_image_clone = hangman_image.clone();
+        let letter_box: gtk::ComboBox = builder.get_object("letter_box").unwrap();
+        // let hangman_image_clone = hangman_image.clone();
 
-        let word_list = vec![
-            "opposed",
-            "consider",
-            "relationship",
-            "tired",
-            "flag",
-            "peasant",
-            "relate",
-            "loop",
-            "tragedy",
-            "wonder",
+        // Define score
+        let score = Rc::new(Cell::new(0));
+
+        // Define number of taking letter
+        let give_letter = Rc::new(Cell::new(3));
+
+        // Define word list
+        let word_list:Vec<String> = vec![
+            "opposed".to_string(),
+            "consider".to_string(),
+            "relationship".to_string(),
+            "tired".to_string(),
+            "flag".to_string(),
+            "peasant".to_string(),
+            "relate".to_string(),
+            "loop".to_string(),
+            "tragedy".to_string(),
+            "wonder".to_string(),
         ];
+        
+        let mut test = Word{
+            word_list,
+        };
 
-        let mut rng = rand::thread_rng();
-        word_label.set_text(&word_list[rng.gen_range(0..10)]);
+        let a = &test.get_word();
+        let selected_word = a[0].clone();
+        let copy_word = a[1].clone();
 
-        check_button.connect_clicked(move |_| {
-            let mut point = point_label.get_text().parse::<u32>().unwrap();
-            let mut world_list_clone = word_list.clone();
+        
 
-            let message_gstring = text_entry.get_text();
-            let message = if message_gstring.as_str().is_empty() {
-                "Please enter a guess!"
+        // Set word label as selected word
+        word_label.set_text(&selected_word);
+
+        check_button.connect_clicked(clone!(@strong score, @strong selected_word, @strong copy_word => move |_| {
+
+            // Check if there is any word left in vector
+            if 0 == 1 {
+                score_label.set_text("YOU WON!");
+                text_entry.hide();
             } else {
-                message_gstring.as_str()
-            };
+                let message_gstring = text_entry.get_text();
 
-            if message_gstring == word_label.get_text() {
-                point = &point + 10;
-                point_label.set_label(&point.to_string());
-                hangman_image_clone.set_from_file("./images/hangmanAlive.png");
+                if message_gstring.as_str().is_empty() {
+                    text_entry.set_placeholder_text(Some("Please enter a guess!"));
+                } else {
+                    if message_gstring == copy_word {
+                        score.set(score.get() + 10);
+    
+                        point_label.set_label(&score.get().to_string());
+    
+                        //let mut rng = rand::thread_rng();
+                        //word_label.set_text(&word_list[rng.gen_range(0..word_list.len())]);
+    
+                        let words = &test.get_word();
+                        selected_word. = words[0].clone();
+                        let copy_word = words[1].clone();
 
-                let mut rng = rand::thread_rng();
-                world_list_clone.remove(
-                    world_list_clone
-                        .iter()
-                        .position(|x| *x == word_label.get_text())
-                        .unwrap(),
-                );
+                        word_label.set_text(&selected_word.clone());
 
-                for i in &world_list_clone {
-                    println!("{}", i)
-                }
-
-                word_label.set_text(&world_list_clone[rng.gen_range(0..10)]);
-            } else {
-                hangman_image_clone.set_from_file("./images/hangmanDead.png");
+                        hangman_image.set_from_file("./images/hangmanAlive.png");
+                    } else {
+                        hangman_image.set_from_file("./images/hangmanDead.png");
+                        text_entry.set_text("");
+                        text_entry.set_placeholder_text(Some("Try again!"));
+                    }
+                };
             }
-            text_entry.set_placeholder_text(Some(message));
-            println!("{}", point.to_string());
-        });
+        }));
 
         window.show_all();
     });
     app.run(&env::args().collect::<Vec<_>>());
+}
+
+struct Word {
+    word_list: Vec<String>,
+}
+
+impl Word {
+    pub fn get_word(&self) -> [String; 2]{
+        //Get the first word from vector
+        let mut rng = rand::thread_rng();
+        let mut selected_word = self.word_list[rng.gen_range(0..10)].clone();
+        let copy_word = selected_word.clone();
+        let word_length = selected_word.len();
+        
+
+        // Number of missing letters from a word
+        let mut missing_letter_number:usize = rng.gen_range(1..word_length - 1);
+
+        println!("selected_word = {}, missing_letter_number = {}, word_len = {}", 
+            selected_word, missing_letter_number, selected_word.len());
+        
+        // Replace random characters with _
+        while missing_letter_number > 0 {
+            // Get random number between 1 and length of the selected word
+            let random_number = rng.gen_range(1..word_length);
+
+            // Replace character which in random_number - 1, random_number range
+            selected_word.replace_range(random_number - 1..random_number, "_");
+            missing_letter_number = missing_letter_number - 1;
+        }
+        
+        // Return changed and original word
+        [selected_word, copy_word] 
+    }
 }
